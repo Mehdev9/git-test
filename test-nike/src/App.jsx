ea02e197b7e54a37410668ed2f4689e4e61df412
Mehdi.BELKATEB <mehdi-belkateb@hotmail.com>
2024-04-10 11:12:43 +0200

    1

--- test-nike/src/App.jsx
+++ test-nike/src/App.jsx
@@ -1,16 +1,25 @@
-import Product from "./product";
+// import Product from "./product";
+import { useState } from "react";
 
 const App = () => {
-  const product = {
-    name: "nike",
-    price: "180 €",
-    description: "loretrlmgkrtkhjnrthonjrohjnryohnryohnryhj",
-    stock: 50+" pcs",
-  };
+  // const product = {
+  //   name: "nike",
+  //   price: "180 €",
+  //   description: "loretrlmgkrtkhjnrthonjrohjnryohnryohnryhj",
+  //   stock: 50+" pcs",
+  // };
+
+  // return (
+  //   <div>
+  //     <Product data = {product} />
+  //   </div>
+  // );
+
+  const [count] = useState(0);
 
   return (
     <div>
-      <Product data = {product} />
+      <p>Counter: {count}</p>
     </div>
   );
 };
