5177aa465ed9456def90394ca62b3c9143fc0cf2
Mehdi.BELKATEB <mehdi-belkateb@hotmail.com>
2024-04-11 12:11:05 +0200

    debut

---
+++ testreact/src/App.jsx
@@ -0,0 +1,46 @@
+
+import './App.css'
+
+function App() {
+
+  
+  const products = [
+    {category: "Sporting Goods", price: "$49.99",  stocked: true, name: "Football"},
+    {category: "Sporting Goods", price: "$9.99",  stocked: true, name: "Baseball"},
+    {category: "Sporting Goods", price: "$29.99", stocked: true, name: "Basketball"},
+    {category: "Electronics", price: "$99.99",  stocked: true, name: "iPod Touch"},
+    {category: "Electronics", price: "$399.99", stocked: true, name: "iPhone 5"},
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+    ];
+
+    // return (
+    //     <div>
+    //       <h1>shop</h1>
+    //       <ul>
+    //         {products.map((number, price) => {
+    //           return <li key={price}>
+    //             {number}
+    //             </li>
+    //         })}
+    //       </ul>
+    //     </div>
+    //   )
+
+    return (
+        <div className='all'>
+          <h2>Our products : </h2>
+          {products.map((product) => (
+            <table className="table table-hover" key={product.id}>
+              <tr>
+              <th>{product.name}</th>
+              <th>{product.price}</th>
+              </tr>
+         </table>
+         ))}
+        </div> 
+      )
+
+
+}
+
+export default App
