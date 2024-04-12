5b6c4b3637e0e169c335d7e613c0727ff9940481
Mehdi.BELKATEB <mehdi-belkateb@hotmail.com>
2024-04-12 16:59:32 +0200

    post

--- dform/src/TestAxios.jsx
+++ dform/src/TestAxios.jsx
@@ -4,32 +4,44 @@ import { useEffect, useState } from "react";
 const TestAxios = () => {
 
 
-  const [product, setProduct] = useState();
+  // const [product, setProduct] = useState();
 
-  useEffect(() => {
-    Axios.get("https://fakestoreapi.com/products/1")
-    .then((response) => {
-      setProduct(response.data.value);
-  })
+  // useEffect(() => {
+  //   Axios.get("https://fakestoreapi.com/products/1")
+  //   .then((response) => {
+  //     setProduct(response.data.value);
+  // })
 
-  }, []);
+  // }, []);
 
-  if(product === undefined) {
-    return(
-      <div>
-        <p>loading...</p>
-      </div>
-    )
-  }
+  // if(product === undefined) {
+  //   return(
+  //     <div>
+  //       <p>loading...</p>
+  //     </div>
+  //   )
+  // }
+
+  const createUser = () => {
+    const userToCreate = {
+      name: "toto",
+  };
+
+  Axios.post("https://jsonplaceholder.typicode.com/users", userToCreate)
+  .then((response) => {
+    console.log(response);
+  });
 
 
     return (
     
       <div>
-        <h1>{product.title}</h1>
+        <h1>FAKE formulaire</h1>
+        <button onClick={() => createUser()}>valider</button>
         
       </div>
     );
   };
+}
 
 export default TestAxios;
